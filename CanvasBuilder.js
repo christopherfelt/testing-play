7386a9b46108f6fe4503b3acd55c8272ad26ef60
christopherfelt <chris.h.felt@gmail.com>
2021-03-22 08:01:28 -0600

    Created paths and started on boundaries

--- CanvasBuilder.js
+++ CanvasBuilder.js
@@ -3,8 +3,12 @@ class CanvasBuilder {
         this.ctx = context;
         this.x = 0;
         this.y = 0;
-        this.width = 0;
-        this.height = 0;
+        this.width = 100;
+        this.height = 100;
+        this.gridWidth = 4;
+        this.gridHeight = 4;
+        this.gridSize = this.gridWidth * this.gridHeight;
+        this.boundaryCells = [];
     }
 
     buildGrid(){
@@ -26,15 +30,15 @@ class CanvasBuilder {
     buildPath(){
         let x_counter = 0;
         let y_counter = 0;
+        
+        this.determineBoundary();
 
         for(let i = 0; i < 16; i++){
             //random
-            
             this.x = x_counter * 100;
             this.y = y_counter * 100;
             
-            // this.drawStraightPath();
-            this.drawStraightPathVertical();
+            this.buildPathGridCell();
 
             x_counter++;
     
@@ -47,8 +51,81 @@ class CanvasBuilder {
 
     }
 
+    determineBoundary(){
+        let x_counter = 1;
+        let y_counter = 1;
+        for(let i = 1; i <= this.gridSize; i++){
+            if(y_counter == 1 || x_counter == this.gridWidth || x_counter == 1 || i > (this.gridSize-this.gridWidth)){
+                console.log("(" + x_counter + ", " + y_counter, ") i: " + i);
+                
+                this.boundaryCells.push(i);
+            }
+            if(x_counter==this.gridWidth){
+                y_counter++;
+                x_counter=0;
+            }
+            x_counter++;
+        }
+        console.log(this.boundaryCells);
+    }
+
     buildPathGridCell(){
-        //switch
+        let randomInt = Math.floor(Math.random()*Math.floor(11));
+        switch(randomInt){
+            // Straight Path Horizontal
+            case 0:
+                this.drawStraightPathHorizontal();
+                break;
+            // Straight Path Vertical
+            case 1:
+                this.drawStraightPathVertical();
+                break;
+            // Cross Path
+            case 2:
+                this.drawStraightPathHorizontal();
+                this.drawStraightPathVertical();
+                break;
+            // Horizontal Top Path
+            case 3:
+                this.drawStraightPathHorizontal();
+                this.drawTopHalfPath();
+                break;
+            // Horizontal Bottom Path
+            case 4:
+                this.drawStraightPathHorizontal();
+                this.drawBottomHalfPath();
+                break;
+            // Vertical Right Path
+            case 5:
+                this.drawStraightPathVertical();
+                this.drawRightHalfPath();
+                break;
+            // Vertical Left Path
+            case 6:
+                this.drawStraightPathVertical();
+                this.drawLeftHalfPath();
+                break;
+            // Right Bottom Corner Path
+            case 7:
+                this.drawRightHalfPath();
+                this.drawBottomHalfPath();
+                break;
+            // Right Top Corner Path
+            case 8:
+                this.drawRightHalfPath();
+                this.drawTopHalfPath();
+                break;
+            // Left Bottom Corner Path
+            case 9:
+                this.drawLeftHalfPath();
+                this.drawBottomHalfPath();
+                break;
+            // Left Top Corner Path
+            case 10:
+                this.drawLeftHalfPath();
+                this.drawTopHalfPath();
+                break;
+        }
         
     }
 
@@ -73,8 +150,8 @@ class CanvasBuilder {
     }
 
     drawRightHalfPath(){
-        var moveToYCoors = this.y + height/2;
-        var lineToXCoors = this.x + width/2;
+        var moveToYCoors = this.y + this.height/2;
+        var lineToXCoors = this.x + this.width/2;
 
         this.ctx.beginPath();
         this.ctx.moveTo(this.x, moveToYCoors);
@@ -84,9 +161,9 @@ class CanvasBuilder {
     }
 
     drawLeftHalfPath(){
-        var moveToXCoors = this.x + width/2
-        var moveToYCoors = this.y + height/2;
-        var lineToXCoors = this.x + width;
+        var moveToXCoors = this.x + this.width/2
+        var moveToYCoors = this.y + this.height/2;
+        var lineToXCoors = this.x + this.width;
 
         this.ctx.beginPath();
         this.ctx.moveTo(moveToXCoors, moveToYCoors);
@@ -95,8 +172,8 @@ class CanvasBuilder {
     }
 
     drawTopHalfPath(){
-        var moveToXCoors = this.x + width/2;
-        var lineToYCoors = this.y + height/2;
+        var moveToXCoors = this.x + this.width/2;
+        var lineToYCoors = this.y + this.height/2;
 
         this.ctx.beginPath();
         this.ctx.moveTo(moveToXCoors, this.y);
@@ -105,8 +182,8 @@ class CanvasBuilder {
     }
 
     drawBottomHalfPath(){
-        var moveToXCoors = this.x + width/2;
-        var moveToYCoors = this.y + height/2;
+        var moveToXCoors = this.x + this.width/2;
+        var moveToYCoors = this.y + this.height/2;
         var lineToYCoors = this.y + this.height;
 
         this.ctx.beginPath();
@@ -114,15 +191,6 @@ class CanvasBuilder {
         this.ctx.lineTo(moveToXCoors, lineToYCoors);
         this.ctx.stroke();
 
-    }
-
-
-    buildTest(){
-        this.ctx.moveTo(0,0);
-        this.ctx.lineTo(200, 100);
-        this.ctx.moveTo(200, 100);
-        this.ctx.lineTo(200, 200);
-        this.ctx.stroke();
     }
 
 }
--- app.js
+++ app.js
@@ -8,49 +8,10 @@ function main(){
     
     let cb = new CanvasBuilder(ctx);
     cb.buildGrid();
-    // cb.buildTest();
-
     cb.buildPath();
-
-    // generateGrid(ctx);
 
 }
 
 main();
 
 
-
-// function test(ctx){
-//     ctx.moveTo(0,0);
-//     ctx.lineTo(200, 100);
-//     ctx.moveTo(200, 100);
-//     ctx.lineTo(200, 200);
-//     ctx.stroke();
-
-//     ctx.strockRect()
-// }
-
-
-
-// function generateGrid(ctx){
-
-//     let x =0;
-//     let y=0;
-//     for(let i = 0; i < 16; i++){
-
-//         ctx.strokeRect(x*100, y*100, 100, 100);
-//         x++;
-
-//         if( x==4 ){
-//             x=0,
-//             y++;
-//         }
-
-//     }
-// }
-
-
-
-// Make a maze
-// Create AI
-// Create player
